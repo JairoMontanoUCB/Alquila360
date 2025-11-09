9ea439c49e99207df2d76a827b9424c1cbdea251
JairoMontanoUCB <jairo.montano@ucb.edu.bo>
2025-11-09 12:03:45 -0400

    base de datos creada

--- alquila360-backend/src/data-source.ts
+++ alquila360-backend/src/data-source.ts
@@ -1,6 +1,12 @@
 import e from "express";
 import { DataSource } from "typeorm";
-import {User} from "./entity/user entity";
+import {User} from "./entity/user.entity";
+import { Propiedad } from "./entity/propiedad.entity";
+import { Contrato } from "./entity/contrato.entity";
+import { Pago } from "./entity/pago.entity";
+import { Ticket } from "./entity/ticket.entity";
+import { PropiedadFoto } from "./entity/propiedad_foto.entity";
+import { TicketFoto } from "./entity/ticket_foto.entity";
 
 import "reflect-metadata"
 
@@ -12,7 +18,7 @@ const AppDataSource = new DataSource({
     username: "alquila360_admin",
     password: "123456789",
     database: "alquila360",
-    entities: [User],
+    entities: [User,Propiedad,Contrato,Pago,Ticket,PropiedadFoto,TicketFoto,],
     synchronize: true,
     logging: true,
 })
