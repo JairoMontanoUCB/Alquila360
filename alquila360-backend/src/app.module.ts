dca8a215ab06df3b87f7977402ea7402af6b15f1
JairoMontanoUCB <jairo.montano@ucb.edu.bo>
2025-11-10 21:53:32 -0400

    Jairo:crud creado de las entidades contrato y propiedad

--- alquila360-backend/src/app.module.ts
+++ alquila360-backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PropiedadModule } from './propiedad/propiedad.module';
+import { ContratoModule } from './contrato/contrato.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,PropiedadModule,ContratoModule],
   controllers: [AppController],
   providers: [AppService],
 })
