98d353b512692cdc961c23bfe01ea2b7218f7aa5
JairoMontanoUCB <jairo.montano@ucb.edu.bo>
2025-11-12 20:52:14 -0400

    propiedad y ticket ya puede guardar fotos

--- alquila360-backend/src/propiedad/propiedad.service.ts
+++ alquila360-backend/src/propiedad/propiedad.service.ts
@@ -1,31 +1,60 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import AppDataSource from "src/data-source";
 import { Propiedad } from "src/entity/propiedad.entity";
-
+import { PropiedadFoto } from "src/entity/propiedad_foto.entity";
 
 @Injectable()
 export class PropiedadService {
-    async createPropiedad(propiedad:Propiedad)
-    {
-        return await AppDataSource.getRepository(Propiedad).save(propiedad);
+
+    async createPropiedad(data: any) {
+
+        
+        const propiedad = await AppDataSource.getRepository(Propiedad).save({
+            direccion: data.direccion,
+            ciudad: data.ciudad,
+            tipo: data.tipo,
+            estado: data.estado,
+            descripcion: data.descripcion,
+            precio_referencia: data.precio_referencia,
+            propietario: { id: data.propietarioId }
+        });
+
+        
+        if (data.fotos && data.fotos.length > 0) {
+
+            const fotoRepo = AppDataSource.getRepository(PropiedadFoto);
+
+            const fotosAInsertar = data.fotos.map((ruta: string) => ({
+                ruta_foto: ruta,
+                propiedad: propiedad  
+            }));
+
+            await fotoRepo.save(fotosAInsertar);
+        }
+
+        return propiedad;
     }
 
-    async getAllPropiedad()
-    {
-        return await AppDataSource.getRepository(Propiedad).find();
+
+    async getAllPropiedad() {
+        return await AppDataSource.getRepository(Propiedad).find({
+            relations: ["fotos", "propietario"]
+        });
     }
-    async getPropiedadById(id:number)
-    {
-        return await AppDataSource.getRepository(Propiedad).findOneBy({id}); 
+
+    async getPropiedadById(id: number) {
+        return await AppDataSource.getRepository(Propiedad).findOne({
+            where: { id },
+            relations: ["fotos", "propietario"]
+        });
     }
-    async updatePropiedad(id:number, propiedadData : Partial<Propiedad>)
-    {
-        return await AppDataSource.getRepository(Propiedad).update(id, propiedadData);
+
+    async updatePropiedad(id: number, propiedadData: Partial<Propiedad>) {
+        await AppDataSource.getRepository(Propiedad).update(id, propiedadData);
         return this.getPropiedadById(id);
     }
-    async deletePropiedad(id:number)
-    {
+
+    async deletePropiedad(id: number) {
         return await AppDataSource.getRepository(Propiedad).delete(id);
     }
-
 }
--- alquila360-backend/src/ticket/ticket.service.ts
+++ alquila360-backend/src/ticket/ticket.service.ts
@@ -1,21 +1,63 @@
 import { Inject, Injectable } from "@nestjs/common";
 import AppDataSource from "src/data-source";
 import { Ticket } from "src/entity/ticket.entity";
+import { TicketFoto } from "src/entity/ticket_foto.entity";
 
 @Injectable()
 export class TicketService {
-    async createTicket(ticket:Ticket)
+    async createTicket(data: any)
     {
-        return await AppDataSource.getRepository(Ticket).save(ticket);
+        const ticket = await AppDataSource.getRepository(Ticket).save({
+            descripcion: data.descripcion,
+            prioridad: data.prioridad,
+            estado: data.estado,
+            fecha_limite: data.fecha_limite,
+            propiedad: { id: data.propiedadId },
+            contrato: { id: data.contratoId },
+            inquilino: { id: data.inquilinoId },
+            tecnico_asignado: { id: data.tecnicoAsignadoId }
+        });
+
+        
+        if (data.fotos && data.fotos.length > 0) {
+
+            const fotoRepo = AppDataSource.getRepository(TicketFoto);
+
+            const fotosAInsertar = data.fotos.map((ruta: string) => ({
+                ruta_foto: ruta,
+                ticket: ticket
+            }));
+
+            await fotoRepo.save(fotosAInsertar);
+        }
+
+        return ticket;
     }
 
     async getAllTicket()
     {
-        return await AppDataSource.getRepository(Ticket).find();
+        return await AppDataSource.getRepository(Ticket).find({
+  relations: [
+    "propiedad",
+    "contrato",
+    "inquilino",
+    "tecnico_asignado",
+    "fotos"
+  ]
+});
     }
     async getTicketById(id:number)
     {
-        return await AppDataSource.getRepository(Ticket).findOneBy({id}); 
+        return await AppDataSource.getRepository(Ticket).findOne({
+            where: { id },
+            relations: [
+                "propiedad",
+                "contrato",
+                "inquilino",
+                "tecnico_asignado",
+                "fotos"
+            ]
+        }); 
     }
     async updateTicket(id:number, ticketData : Partial<Ticket>)
     {
