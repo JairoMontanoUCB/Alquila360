583408db1541cc6f18be4159c75ac40c8962af4c
JairoMontanoUCB <jairo.montano@ucb.edu.bo>
2025-11-14 00:17:35 -0400

    ya esta el login conectado a al backend

--- alquila360-backend/src/app.module.ts
+++ alquila360-backend/src/app.module.ts
@@ -6,12 +6,13 @@ import { PropiedadModule } from  './propiedad/propiedad.module'
 import { ContratoModule } from './contrato/contrato.module';
 import { TicketModule } from './ticket/ticket.module';
 import { PagoModule } from './pago/pago.module';
-
 import { PdfModule } from './pdf/pdf.module';
+import { authPlugins } from 'mysql2';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UserModule,PropiedadModule,ContratoModule,TicketModule,PagoModule,PdfModule],
+  imports: [UserModule,PropiedadModule,ContratoModule,TicketModule,PagoModule,PdfModule,AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
